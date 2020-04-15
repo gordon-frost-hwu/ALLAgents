42ff7e74d16fe6f95fd74b292b0a4587c598149f
Gordon Frost <gordon.frost.hwu@googlemail.com>
2020-04-15 20:00:54 +0100

    Cleaned up CACLA agent by removing commented out or unused code and corrected comments

--- agent.py
+++ agent.py
@@ -3,26 +3,28 @@ from torch.nn.functional import mse_loss
 from all.logging import DummyWriter
 from torch.distributions.normal import Normal
 from all.agents import Agent
-from all import nn
-import models
-import numpy as np
 
 class CACLA(Agent):
     '''
-    Vanilla Actor-Critic (VAC).
-    VAC is an implementation of the actor-critic alogorithm found in the Sutton and Barto (2018) textbook.
-    This implementation tweaks the algorithm slightly by using a shared feature layer.
-    It is also compatible with the use of parallel environments.
-    https://papers.nips.cc/paper/1786-actor-critic-algorithms.pdf
+    Continuous Actor Critic Learning Automation (CACLA).
+    CACLA is an implementation of the CACLA alogorithm found in van Hasselt and Wiering (2007).
+    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.75.7658&rep=rep1&type=pdf
+    This implementation tweaks the algorithm slightly by only updating weights at the end of an episode
+    and using a replay buffer to collect interaction samples. At the end of the episode, the replay buffer
+    gets sampled n_iter times to train the critic, and then separately another n_iter times to train actor
+    A shared feature layer can also be used, but successful results did not need it
 
     Args:
-        features (FeatureNetwork): Shared feature layers.
+        features (FeatureNetwork): Shared feature layers. If None, raw environment state vector is used
         v (VNetwork): Value head which approximates the state-value function.
         policy (DeterministicPolicy): Policy head which outputs an action distribution.
         buffer (ExperienceBuffer): buffer for experience replay.
         discount_factor (float): Discount factor for future rewards.
-        n_envs (int): Number of parallel actors/environments
-        n_steps (int): Number of timesteps per rollout. Updates are performed once per rollout.
+        sigma (float): initial variance of Normal distribution used for exploration
+        sigma_decay (float): decay rate for exploration
+        sigma_min (float): minimum value for exploration
+        n_iter (int): Number of times to sample the replay buffer when training
+        minibatch_size (int): Number of timesteps sampled per n_iter of training
         writer (Writer): Used for logging.
     '''
     def __init__(self, features, v, policy, buffer, action_space,
@@ -32,8 +34,6 @@ class CACLA(Agent):
                  sigma_min=0.1,
                  n_iter=100,
                  minibatch_size=32,
-                 update_frequency=1,
-                 replay_start_size=1,
                  writer=DummyWriter()):
         self.features = features
         self.v = v
@@ -47,80 +47,39 @@ class CACLA(Agent):
         self.sigma_min = sigma_min
         self.n_iter = n_iter
         self._features = None
-        self._distribution = None
         self._action = None
-        self._exploration = None
         self._state = None
         self._action_low = torch.tensor(action_space.low, device=policy.device)
         self._action_high = torch.tensor(action_space.high, device=policy.device)
-        print("action low: {0}".format(self._action_low))
-        print("action high: {0}".format(self._action_high))
-        self.log_prob = None
-        self._last_features = None
-        self._frames_seen = 0
-        self.update_frequency = update_frequency
-        self.replay_start_size = replay_start_size
-        # self.gaussian = Normal(0, noise * torch.tensor((action_space.high - action_space.low) / 2).to(policy.device))
 
     def _normal(self, output):
-        # return Normal(output, torch.tensor([self.sigma]).to('cuda'))
-        # noise = self.sigma * torch.tensor((self._action_high - self._action_low) / 2).to('cuda')
         self.writer.add_scalar("sigma", self.sigma)
         return Normal(output, self.sigma)
 
     def act(self, state, reward):
-        self._train(state, reward)
-        # self.writer.add_scalar("state/0", state.raw[0][0])
-        # self.writer.add_scalar("state/1", state.raw[0][1])
-        # self.writer.add_scalar("state/2", state.raw[0][2])
-        # self.writer.add_scalar("state/3", state.raw[0][3])
-        # print("state: {0}".format(state.raw))
+        self._train(state)
         self.replay_buffer.store(self._state, self._action, reward, state)
-
         self._state = state
         self._action = self._choose_action(state)
-
         return self._action
 
     def _choose_action(self, state):
+        # If a feature ANN is provided, use it, otherwise raw state vector is used
         self._features = self.features(state) if self.features is not None else state
-
-        # if self._last_features is not None:
-        #     print("features: {0}".format(self._features.raw - self._last_features))
-        # self._distribution = Normal(self.policy(self._features), 0)
-
-        # deterministic_action, self.log_prob = self.policy.eval(self._features)
-        # print("det action: {0}".format(deterministic_action.data))
-        # print("log prob: {0}".format(self.log_prob))
         deterministic_action = self.policy.eval(self._features)
+        # uncomment to log the policy output
+        # self.writer.add_scalar("action/det", deterministic_action)
 
-        self.writer.add_scalar("action/det", deterministic_action)
+        # Get the stochastic action by centering a Normal distribution on the policy output
+        stochastic_action = self._normal(deterministic_action).sample()
 
-        self._distribution = self._normal(deterministic_action)
-
-        # TODO -
-        # if self._frames_seen % 5 == 0 or self._frames_seen == 1:
-        stochastic_action = self._distribution.sample()
-
-        self._exploration = stochastic_action - deterministic_action
-        # print("exploration: {0}".format(self._exploration))
-
-        # self.log_prob -= torch.log(1 - action.pow(2) + 1e-6)
-        # self.log_prob = self.log_prob.sum(1)
-
-        # print("deterministic action: {0}".format(deterministic_action.data))
-        # print("exploration   action: {0}".format(action.data))
-
-        # stochastic_action = deterministic_action + self._exploration
-
-        # print(type(self.writer))
-        # print("stochastic action: {0}".format(self._action.data))
+        # Clip the stochastic action to the gym environment's action space
         stochastic_action = torch.max(stochastic_action, self._action_low)
         stochastic_action = torch.min(stochastic_action, self._action_high)
-        self.writer.add_scalar("action/sto", stochastic_action)
         return stochastic_action
 
-    def _train(self, states, rewards):
+    def _train(self, states):
+        # only train (update weights) at the end of an episode; i.e. at a terminal state
         if states.done:
             for i in range(self.n_iter):
                 _, values, targets, _ = self.generate_targets()
@@ -132,44 +91,20 @@ class CACLA(Agent):
             for i in range(self.n_iter):
                 features, values, targets, actions = self.generate_targets()
                 greedy_actions = self.policy(features)
-                self.log_prob = self._distribution.log_prob(greedy_actions)
-                # self.update_actor_ac(values=values, targets=targets)
                 self.update_actor_cacla(values=values, targets=targets, greedy_actions=greedy_actions, actions=actions)
 
+            # Decay the exploration
             if self.sigma > self.sigma_min:
                 self.sigma *= self.sigma_decay
 
-    def update_actor_ac(self, targets, values):
-        # advantages = targets[0] - values.detach()[0]
-        advantages = targets - values.detach()
-        # print("type of adv and log_prob: {0} {1}".format(type(advantages), type(self.log_prob)))
-        # self.writer.add_scalar("advantages", advantages)
-        # compute losses
-        # print(advantages)
-        # print(self.log_prob)
-        policy_loss = -(advantages * self.log_prob).mean()
-        policy_loss = policy_loss.requires_grad_(True)       # if uncommented, policy output always zero!!
-        # print(policy_loss.data)
-        if not torch.isnan(policy_loss):
-            self.policy.reinforce(policy_loss)
-        else:
-            print("policy loss is NaN")
-
     def update_actor_cacla(self, targets, values, greedy_actions, actions):
+        # calculate TDE
         advantages = targets - values.detach()
-        exploration = actions - greedy_actions
 
+        # Get the indexes where the TDE is positive (i.e. the action resulted in a good state transition)
         idx = torch.where(advantages > 0.0)[0]
-        # print("exploration: {0}".format(exploration))
-        # print("idx: {0}".format(idx))
-        # print("len(idx): {0}".format(len(idx)))
-
-        # policy_loss = policy_loss.clone().detach().requires_grad_(True)       # if uncommented, policy output always zero!!
-        # print(policy_loss.data)
         if len(idx) > 0:
-            # policy_loss = -(exploration[idx]).mean()    # * self.log_prob[idx]).mean()
             policy_loss = mse_loss(greedy_actions[idx], actions[idx])
-            # print("policy loss: {0}".format(policy_loss))
 
             if not torch.isnan(policy_loss):
                 self.policy.reinforce(policy_loss)
@@ -182,34 +117,21 @@ class CACLA(Agent):
 
         # forward pass
         features = self.features(states) if self.features is not None else states
-        # pi_features = self.features(state)
         values = self.v(features)
-        # pi_values = self.v(pi_features)
-        if not torch.isnan(values[0]):
-            self.writer.add_scalar("statevalue", values[0])
 
         # compute targets
         features_next_states = self.features.target(next_states) if self.features is not None else next_states
 
+        # compute state_{t+1} value
         next_state_value = self.v.target(features_next_states)
-        # print("state_t value: {0}, state_t_plus_1 value: {1}".format(values[0], next_state_value[0]))
         targets = rewards + self.discount_factor * next_state_value
-
-
-
         return features, values, targets, actions
 
     def update_critic(self, values, targets):
-        # for param in self.policy.model.parameters():
-        #     print(param.data)
-        # print("policy weights before: {0}".format(self.policy.model.parameters()))
         # backward pass
         value_loss = mse_loss(values, targets)
         self.v.reinforce(value_loss)
+
+        # If feature ANN given, do a backwards pass over it too (taken from VAC agent)
         if self.features is not None:
             self.features.reinforce()
-        # idx += 1
-
-    def _should_train(self):
-        self._frames_seen += 1
-        return self._frames_seen > self.replay_start_size and self._frames_seen % self.update_frequency == 0
