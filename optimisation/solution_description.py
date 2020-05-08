4ae55b8065f9d5e84da135f97ec0c48bcb2b8223
Gordon Frost <gordon.frost.hwu@googlemail.com>
2020-05-08 10:04:14 +0100

    extended solution description with an absolute tolerance field. Can be a numpy array of shape (num_genes, )

--- optimisation/solution_description.py
+++ optimisation/solution_description.py
@@ -3,7 +3,7 @@
 
 class SolutionDescription(object):
     def __init__(self, num_genes, gene_bounds, gene_init_range,
-                 gene_sigma, gene_mut_probability):
+                 gene_sigma, gene_mut_probability, atol):
         # assert all(isinstance(elem, list) and len(elem) == 2 for elem in gene_bounds),
         #               "gene_bounds must be a list of list"
         assert gene_bounds.shape == (num_genes, 2), \
@@ -14,6 +14,8 @@ class SolutionDescription(object):
             "gene_sigma must be numpy array of shape: ({0}, )".format(num_genes)
         assert gene_mut_probability.shape == (num_genes, ), \
             "gene_mut_probability must be numpy array of shape: ({0}, )".format(num_genes)
+        assert atol.shape == (num_genes,), \
+            "atol must be numpy array of shape: ({0}, )".format(num_genes)
 
         self.num_genes = num_genes
         self.gene_bounds = gene_bounds
@@ -21,3 +23,4 @@ class SolutionDescription(object):
         # TODO - compute the signma from bounds?
         self.gene_sigma = gene_sigma
         self.gene_mutation_prob = gene_mut_probability
+        self.atol = atol
