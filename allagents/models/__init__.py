938bdf581f82fd2a5b0fe5958193ad1748892bc6
Gordon Frost <gordon.frost.hwu@googlemail.com>
2020-11-11 10:36:12 +0000

    WIP. Experimenting with diff network topologies after nessie_basic.py script fixes

--- allagents/models/__init__.py
+++ allagents/models/__init__.py
@@ -11,34 +11,33 @@ import torch.nn
 # for how to initialise weights
 # https://stackoverflow.com/questions/49433936/how-to-initialize-weights-in-pytorch/49433937#49433937
 
+
 def init_weights(m):
     if type(m) == nn.Linear:
         torch.nn.init.xavier_uniform(m.weight)
         m.bias.data.fill_(0.001)
 
-def actor(env, hidden1=400, hidden2=300):
+
+def create_net(input_dim, output_dim, hidden1, hidden2):
     net = nn.Sequential(
-        nn.Linear(env.state_space.shape[0], hidden1),
+        nn.Linear(input_dim, hidden1),
         nn.ReLU(),
         nn.Linear(hidden1, hidden2),
         nn.ReLU(),
-        nn.Linear(hidden2, env.action_space.shape[0])
+        nn.Linear(hidden2, output_dim)
     )
     net.apply(init_weights)
     net.float()
     return net
 
+
+def actor(env, hidden1=400, hidden2=300):
+    return create_net(env.state_space.shape[0], env.action_space.shape[0], 48, 48)
+
+
 def critic(env, hidden1=400, hidden2=300):
-    net = nn.Sequential(
-        nn.Linear(env.state_space.shape[0], hidden1),
-        nn.ReLU(),
-        nn.Linear(hidden1, hidden2),
-        nn.ReLU(),
-        nn.Linear(hidden2, 1)
-    )
-    net.apply(init_weights)
-    net.float()
-    return net
+    return create_net(env.state_space.shape[0], 1, 48, 48)
+
 
 def features(state_space_size, hidden1=400):
     net = nn.Sequential(
